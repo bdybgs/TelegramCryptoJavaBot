447fdf2b381785a068be50ba5b989903aac53ca6
bdybgs <bondarev.mc.2018@gmail.com>
2024-05-24 00:46:09 +0300

    integrated CoinMarketCapClient for checking cryptocurrency prices

--- src/main/java/com/example/telegrambot/TelegramBot.java
+++ src/main/java/com/example/telegrambot/TelegramBot.java
@@ -1,2 +1,48 @@
-package com.example.telegrambot;public class TelegramBot {
+package com.example.telegrambot;
+
+import com.example.telegrambot.coinmarketcap.CoinMarketCapClient;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import lombok.SneakyThrows;
+import org.json.JSONObject;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    public TelegramBot(DefaultBotOptions options, String botToken) {
+        super(options, botToken);
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            var text = update.getMessage().getText();
+            var chatId = update.getMessage().getChatId();
+            var response = CoinMarketCapClient.getCoinPrice(text.toUpperCase());
+            String message;
+
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                JSONObject data = jsonObject.getJSONObject("data");
+                JSONObject coinData = data.getJSONObject(text.toUpperCase());
+                JSONObject quote = coinData.getJSONObject("quote");
+                JSONObject usdQuote = quote.getJSONObject("USD");
+                double price = usdQuote.getFloat("price");
+                message = "Цена " + text.toUpperCase() + " составляет $" + price;
+            } catch (Exception e) {
+                message = "Ошибка: не удалось получить цену для " + text.toUpperCase();
+            }
+            SendMessage sendMessage = new SendMessage(chatId.toString(), message);
+            sendApiMethod(sendMessage);
+
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Crypto-checker";
+    }
 }
--- src/main/java/com/example/telegrambot/coinmarketcap/CoinMarketCapClient.java
+++ src/main/java/com/example/telegrambot/coinmarketcap/CoinMarketCapClient.java
@@ -1,2 +1,58 @@
-package com.example.telegrambot.Coinmarketcap;public class CoinMarketCapClient {
+package com.example.telegrambot.coinmarketcap;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinMarketCapClient {
+
+    private static String apiKey = "de9a0ef7-1b40-41e6-8ec9-be092e024692";
+
+    public static String getCoinPrice(String symbol) throws IOException, URISyntaxException {
+        String uri = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
+        List<NameValuePair> parameters = new ArrayList<>();
+        parameters.add(new BasicNameValuePair("symbol", symbol));
+        parameters.add(new BasicNameValuePair("convert", "USD"));
+
+        return makeAPICall(uri, parameters);
+    }
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return response_content;
+    }
 }
